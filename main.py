4b237709cd8fd86292009afdc0c2616105dd1bd6
Trillaman <26063289+trillaman@users.noreply.github.com>
2021-09-02 13:21:27 +0200

    Will switch to html parser soon
    I will replace beautifulsoup with html parser because bs it's too large and uneccessary for this project. It was good to fast prototype

--- main.py
+++ main.py
@@ -26,17 +26,30 @@ def write_with_encloses(filename, tag_name, tag_contents):
 def write_without_encloses(filename, tag_name, tag_contents):
   filename.write(tag_name + tag_contents + "\n")
 
+#enabled (0/1) - unordered/ordered
+def write_lists(filename, tag_contents, enabled):
+  start_index = 1
+  if enabled == 0:
+    filename.write("* " + tag_contents + "\n")
+  if enabled == 1:
+    filename.write(str(start_index) + " " + tag_contents + "\n")
+    start_index += 1
+  
+
 html_without_enclosing = {"h1": "# ", "h2": "## ", "h3": "### ", "h4": "#### ","h5": "##### ", "h6": "###### ", 
-                     "hr": "---", "ul": "", "li": "* ", "ol": "",
+                     "hr": "---",
                     }
 html_with_enclosing = {"i": "*", }
+html_lists = {"ul": 0, "ol": 1, "li": ""}
+
 
 '''
         if tag.name == "ul":
           ordered_list_enabled = 1
           if ordered_list_enabled == 1 and tag.contents:
             readme_output.write(ordered_list_number + ". " + tag.contents[0] + "\n")
-'''
+
+''' 
 for tag in bs.find_all():
     ordered_list_enabled = 0
     ordered_list_number = 1
@@ -51,7 +64,23 @@ for tag in bs.find_all():
     elif tag.name in html_with_enclosing:
       if tag.contents:
         write_with_encloses(readme_output, html_with_enclosing[tag.name], tag.contents[0])
-
+    elif tag.name in html_lists:
+      if tag.name == "ul":
+       
+        '''
+        ordered_list_enabled = 0
+        write_lists(readme_output, tag.contents[0], ordered_list_enabled)
+      
+        if tag.contents:
+          readme_output.write("* " + tag.contents[0])
+        '''
+        print("UL FOUND")
+      elif tag.name == "ol":
+        print("OL FOUND")
+        '''
+        ordered_list_enabled = 1
+        write_lists(readme_output, tag.contents[0], ordered_list_enabled)
+        '''
 print("Done")
 
 html_file.close()
--- readme_output.md
+++ readme_output.md
@@ -1,8 +1,3 @@
 ###### Header Title
 ---
 *Project is created with*
-
-
-* 1
-* 2
-* 3
