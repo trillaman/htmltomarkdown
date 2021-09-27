5714119bffca5899538372486e734d82f401266b
Trillaman <26063289+trillaman@users.noreply.github.com>
2021-09-27 10:45:35 +0200

    Moved to main function

--- main.py
+++ main.py
@@ -14,14 +14,12 @@
 #Line Breaks - to do
 #YouTube Videos - to do
 
+#TODO PYTHON SIDE:
+      # exceptions handling
+      # refactoring for cleaner code
+
 from bs4 import BeautifulSoup
 import time
-html_file = open("index.html", "r")
-readme_output = open("readme_output.md", "w")
-
-bsoup = BeautifulSoup(html_file, 'html.parser')
-
-output = bsoup.get_text()
 
 html_without_enclosing = {"h1": "# ", "h2": "## ", "h3": "### ", "h4": "#### ","h5": "##### ", "h6": "###### ", 
                      "hr": "***", "p": "", "img": ""
@@ -60,55 +58,59 @@ def write_italic_bold(filename, tag_name, tag_contents):
 def write_image(filename, image_href, alt_text):
   filename.write("![alt text]" + "(" + image_href + " \"" + alt_text + "\")")
 
-bsoup.prettify()
-print(list(bsoup.children))
-for tag in list(bsoup.children):
-  #THIS VAR IS USED IN  CASE OF I AND B USED IN SAME TIME TO AVOID DUPLICATING
-  bold_itallic_written = 0
-    #write_image(readme_output, image['src'], image['alt'])
-  #below conditional compare dicts and if html_without_enclosing is true than it knows to no to add enclosing signs like *something*
-  #THIS IS FOR NOTENCLOSED TAGS LIKE h1
-  if tag.name in html_without_enclosing:
-    #this writes to file corresponding markdown value for HTML tag
-    if tag.contents:
-      write_without_encloses(readme_output, html_without_enclosing[tag.name], tag.contents[0])        
-    else:
-      if tag.name == "img":
-        write_image(readme_output, tag['src'], tag['alt'])
-      write_without_encloses(readme_output, html_without_enclosing[tag.name], "")
 
-  #BELOW IS FOR ENCLOSED TAGS LIKE "<b>content</b>" 
-  if tag.name in html_with_enclosing:
-    if tag.contents:
-      #print(tag.contents)
-      for ch in tag.contents:
-        #below for <i><b>content</b></i> so italic content can be also bolded
-        if "<b>" in str(ch):
-          content = str(tag.contents[0])
-          content = content.replace("<b>", "")
-          content = content.replace("</b>", "")
-          write_italic_bold(readme_output, html_with_enclosing[tag.name], content)
-        else:
-          write_with_encloses(readme_output, str(html_with_enclosing[tag.name]), str(tag.contents[0]))
 
-  #THIS ONE SEARCH FOR LISTS
-  if tag.name in html_lists:
-    if tag.name == "ul":
-      for li in tag.findAll('li'):
-        if li.contents:
-          write_unordered_list(readme_output, li.contents[0])
+def main():
+  html_file = open("index.html", "r")     #input file
+  readme_output = open("readme_output.md", "w")     #output - readme_output.md
 
-    if tag.name == "ol":
-      index = 1
-      for li in tag.findAll('li'):
-        if li.contents:
-          write_ordered_list(readme_output, index, li.contents[0])
-          index += 1
+  bsoup = BeautifulSoup(html_file, 'html.parser')     #parse input
 
-  if tag.name in html_hrefs:
-    write_links(readme_output, str(tag.get('href')), tag.contents[0])
+  #could be prettier - bsoup.prettify()
+  #print(list(bsoup.children))
+
+  for tag in list(bsoup.children):      #THIS NEED TO BE REFACTORED
+    if tag.name in html_without_enclosing:
+      if tag.contents:
+        write_without_encloses(readme_output, html_without_enclosing[tag.name], tag.contents[0])
+      else: #for empty content - so without text between > and <
+        if tag.name == "img": #check for images
+          write_image(readme_output, tag['src'], tag['alt'])
+        write_without_encloses(readme_output, html_without_enclosing[tag.name], "")     #if tag is not image then write normal tag
+
+    #BELOW IS FOR ENCLOSED TAGS LIKE "<b>content</b>"
+    if tag.name in html_with_enclosing:
+      if tag.contents:
+        for ch in tag.contents:     #ch for child
+          if "<b>" in str(ch):      #for <i><b>content</b></i> so italic content can be also bolded
+            content = str(tag.contents[0])
+            content = content.replace("<b>", "")      #remove opening bold tag to get pure tag content
+            content = content.replace("</b>", "")     #remove closing bold tag to get pure tag content
+            write_italic_bold(readme_output, html_with_enclosing[tag.name], content)      #separated function for bolded italic
+          else:     #if not child but got contents - type enclosed tag in file
+            write_with_encloses(readme_output, str(html_with_enclosing[tag.name]), str(tag.contents[0]))
+
+    #THIS ONE SEARCH FOR LISTS
+    if tag.name in html_lists:
+      if tag.name == "ul":      #unordered
+        for li in tag.findAll('li'):
+          if li.contents:
+            write_unordered_list(readme_output, li.contents[0])
+
+      if tag.name == "ol":      #ordered
+        index = 1
+        for li in tag.findAll('li'):
+          if li.contents:
+            write_ordered_list(readme_output, index, li.contents[0])
+            index += 1
+
+    #THIS ONE FOR HYPERLINKS
+    if tag.name in html_hrefs:
+      write_links(readme_output, str(tag.get('href')), tag.contents[0])
     
-print("\n\nDone")
+  html_file.close()   #close input file after use
+  readme_output.close()     #close output file after use
 
-html_file.close()
-readme_output.close()
+if __name__ == "__main__":
+  main()
+  print("\n\nDone")
